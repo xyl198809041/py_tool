b0fcf2a6cc3035a14fd88c1c8eba7b96902f9c98
xyl <xyl19880904@163.com>
2019-11-30 13:50:28 +0800

    修复依赖错误

--- setup.py
+++ setup.py
@@ -12,6 +12,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/xyl198809041/py_tool",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages('pack'),
     python_requires='>=3.6',
 )
