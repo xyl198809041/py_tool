37a88b8b7846d1b53312a7ca7d66fdc4b0b55bcd
xyl <xyl19880904@163.com>
2019-11-30 13:42:53 +0800

    做成模块

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_tool",  # Replace with your own username
+    version="1.0",
+    author="xyl",
+    author_email="author@example.com",
+    description="我的工具",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xyl198809041/py_tool",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+)
