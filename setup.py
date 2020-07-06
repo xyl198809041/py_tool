99a5b868deba5c1efae7f28fc79275bfcf03dfee
xyl <xyl19880904@163.com>
2020-07-06 20:38:31 +0800

    添加依赖

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     python_requires='>=3.5',
     install_requires=[
+        'wheel',
         'cython',
         'xlrd',
         'selenium',
