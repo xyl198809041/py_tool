9662c32d3c2a53be5247c9b4409d4130963e9d86
xyl19 <xyl19880904@163.com>
2022-06-05 11:47:02 +0800

    语音合成优化

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_tool",  # Replace with your own username
-    version="1.2.6",
+    version="1.2.7",
     author="xyl",
     author_email="author@example.com",
     description="我的工具",
