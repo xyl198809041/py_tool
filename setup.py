3d31cc2112eaaab2f276650e4f02ad175fef9e9f
xyl19 <xyl19880904@163.com>
2023-08-09 15:46:56 +0800

    声音增强

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py_tool",  # Replace with your own username
-    version="1.3.3",
+    version="1.3.4",
     author="xyl",
     author_email="author@example.com",
     description="我的工具",
